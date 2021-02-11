c4670bcf569001123a8581e1ed8d06cb1586c053
Andy @ einkpi <andy@rlmn.co>
2021-02-11 11:02:50 +0000

    centred number and added usage instructions

--- display.py
+++ display.py
@@ -5,25 +5,13 @@ sys.path.append("/home/andy/dev/e-Paper/RaspberryPi_JetsonNano/python")
 import os
 import time
 import epd2in13d
-
-
 #from lib.waveshare_epd import epd2in13d
-
-
-
 from PIL import Image, ImageDraw, ImageFont
-
-
-try:   
-   sys.argv[1:] # do something with sys.argv[1:]  
-except IndexError:  
-   print("usage is...")
-   sys.exit()
-
 
 if len(sys.argv) == 1:
     # Print usage...
-    print("usage")
+    print("usage: ",sys.argv[0], " <temp out> <temp in>")
+    print("   eg: ",sys.argv[0], " -0.7 22.2")
     sys.exit()
 
 print("(temp_out)arg1 = " + sys.argv[1])
@@ -40,27 +28,41 @@ try:
     #display.init(display.lut_full_update)
     display.init()
     #display.Clear()
-    w = display.height
-    h = display.width
-    #print('width:', w)
-    #print('height:', h)
+    W = display.height
+    H = display.width
+    print('screen width:', W)
+    print('screen height:', H)
     ### ... IMAGE CODE ... ###
-    #body = ImageFont.truetype(os.path.join(pic_dir, 'Font.ttc'), 18, index=5)
-    font15 = ImageFont.truetype(os.path.join(pic_dir, 'Font.ttc'), 20)
-    font62 = ImageFont.truetype(os.path.join(pic_dir, 'Font.ttc'), 55)
+    fonttext = ImageFont.truetype(os.path.join(pic_dir, 'Font.ttc'), 20)
+    fontnum = ImageFont.truetype(os.path.join(pic_dir, 'Font.ttc'), 55)
 
-    #imagein = Image.new(mode='1', size=(w, h), color=255)
-    imagein = Image.new(mode='1', size=(h, w), color=255)
+    # width: 212
+    #height: 104
+    imagein = Image.new(mode='1', size=(H, W), color=255)
     drawin = ImageDraw.Draw(imagein)
     drawin.rectangle([(0,106),(104,212)],fill = 0)
-    #drawin.text((0, 0), '99.9', font=font62, fill=0, align='left')
-    drawin.text((0, 10), temp_in, font=font62, fill=0, align='left')
-    drawin.text((0, 80), 'inside', font=font15, fill=0, align='left')
+    #drawin.text((0, 0), '99.9', font=fontnum, fill=0, align='left')
+    x = 0
+    y = 10
 
-    #drawin.text((0, 107), '-0.9', font=font62, fill=1, align='left')
-    drawin.text((0, 117), temp_out, font=font62, fill=1, align='left')
-    drawin.text((0, 188), 'outside', font=font15, fill=1, align='left')
+    draw = ImageDraw.Draw(imagein)
+    #drawin.text(((H-w)/2,(W-h)/2), temp_in, fill=0)
+    w, h = drawin.textsize(temp_in,font=fontnum)
+    drawin.text(((H-w)/2,10), temp_in, fill=0, font=fontnum)
 
+    w, h = drawin.textsize('inside',font=fonttext)
+    #drawin.text(((H-w)/2, 80), 'inside', font=fonttext, fill=0)
+    drawin.text((0, 80), 'inside', font=fonttext, fill=0)
+
+    #drawin.text((0, 107), '-0.9', font=fontnum, fill=1, align='left')
+    #drawin.text((0, 117), temp_out, font=fontnum, fill=1, align='left')
+    w, h = drawin.textsize(temp_out,font=fontnum)
+    drawin.text(((H-w)/2,117), temp_out, fill=1, font=fontnum)
+    #drawin.text((0, 188), 'outside', font=fonttext, fill=1, align='left')
+
+    w, h = drawin.textsize('outside',font=fonttext)
+    #drawin.text(((H-w)/2, 188), 'outside', font=fonttext, fill=1)
+    drawin.text(((H-w), 188), 'outside', font=fonttext, fill=1)
 
     #print("display..")
     #display.display(display.getbuffer(imageb),display.getbuffer(imager))
